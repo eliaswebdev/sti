b4da08d9ef077dd76519d92c630813cb51a96f6b
Elias Ferreira Junior <eliaswebdev@gmail.com>
2015-06-27 21:45:37 -0300

    quick commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :pessoa_fisicas
   resources :pessoa_juridicas
+
+  root 'pessoa_juridicas#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
